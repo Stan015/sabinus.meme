d65c600c0be4f662348d14d28263f8c3a04f4a4e
Stanley Azi <azistanley17@gmail.com>
2024-07-20 06:47:42 +0100

    fixing vercel CLI error

--- src/app/api/favourite-memes/route.ts
+++ src/app/api/favourite-memes/route.ts
@@ -2,20 +2,15 @@ import { v2 as cloudinary } from "cloudinary";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  try {
-    const { resources } = await cloudinary.search
-      .expression(
-        "resource_type:image AND folder:sabinus-memes AND tags=favourite"
-      )
-      .sort_by("uploaded_at", "desc")
-      .with_field("tags")
-      // .max_results(30)
-      .execute();
+  const { resources } = await cloudinary.search
+    .expression(
+      "resource_type:image AND folder:sabinus-memes AND tags=favourite"
+    )
+    .sort_by("uploaded_at", "desc")
+    .with_field("tags")
+    // .max_results(30)
+    .execute();
 
-    // console.log(resources);
-    return NextResponse.json(resources);
-  } catch (error) {
-    console.log(`Error occurred: ${error}`);
-    return NextResponse.error();
-  }
+  // console.log(resources);
+  return NextResponse.json(resources);
 };
