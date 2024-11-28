d83b941a55bb02833b37787f70f97976f44dea95
Stanley Azi <azistanley17@gmail.com>
2024-11-28 09:19:58 +0100

    fix: cn function

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import cn from "@/utils/cn";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -8,7 +8,7 @@ export function Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className={clsx(
+      className={cn(
         "flex h-10 items-center rounded-lg bg-blue px-4 text-sm font-medium text-white transition-colors hover:bg-blue-deep focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-blue active:bg-blue-deep aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
         className,
       )}
