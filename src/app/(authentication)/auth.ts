02df6759a9bdb27ab6ed9492e627c0d197e2f7e2
Stanley Azi <azistanley17@gmail.com>
2024-11-29 17:22:47 +0100

    fixing auth for production

--- src/app/(authentication)/auth.ts
+++ src/app/(authentication)/auth.ts
@@ -71,7 +71,7 @@ export async function signUp(formData: FormData) {
 
 export const handleGoogleSignUp = async (provider: Provider) => {
   const supabase = await createClient();
-  const origin = (headers() as unknown as UnsafeUnwrappedHeaders).get("origin");
+  const origin = (await headers() as unknown as UnsafeUnwrappedHeaders).get("origin");
 
   console.log(origin);
   const { data, error } = await supabase.auth.signInWithOAuth({
