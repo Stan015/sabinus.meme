779ce9089a4e34a56aba03bee6194011794d3e90
Stanley Azi <azistanley17@gmail.com>
2024-11-29 21:32:54 +0100

    add: 404 page and error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,17 +4,17 @@ import { Button } from "@/components/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-export default function ErrorPage() {
+export default function NotFound() {
   const router = useRouter();
 
   return (
     <section className="mt-[7rem] px-[10%] flex flex-col items-center gap-8 w-full min-h-[calc(100dvh-15rem)] mb-[2rem]">
       <h1 className="text-[2rem] max-md:text-[1.5rem] max-sm:text-[1.3rem] font-bold">
-        Something went wrong.
+        404 - Page Not Found.
       </h1>
       <Image
-        src="/media/error-img.JPG"
-        alt="sabinus meme for error page"
+        src="/media/404.JPG"
+        alt="sabinus meme for 404 page"
         width={400}
         height={600}
         className="border-[5px] border-blue rounded-3xl"
