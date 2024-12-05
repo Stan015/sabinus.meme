9f5a5710a418300db0ec09e6a7766b4fc42c7fdb
Stanley Azi <azistanley17@gmail.com>
2024-12-05 22:22:57 +0100

    add: Resend message sending...

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from "@/components/email-template";
 import { NextResponse, type NextRequest } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
